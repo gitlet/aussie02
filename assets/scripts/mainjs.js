33a97770bfc81c446301b41c828cfdbc1cee476e
gitlet <11thdim@gmail.com>
2017-02-24 06:24:41 +0530

    Updating with more functionality

--- assets/scripts/mainjs.js
+++ assets/scripts/mainjs.js
@@ -7,4 +7,43 @@ $(document).ready(function() {
       $('body .menu').slideUp();
     });
   });
+
+  // services html
+  $('body .wrapper .services-list > li#web-dsgn').click(function() {
+    $('body .wrapper .services-list > li#web-dsgn > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#web-dev').click(function() {
+    $('body .wrapper .services-list > li#web-dev > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#gr-dsgn').click(function() {
+    $('body .wrapper .services-list > li#gr-dsgn > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#soft-dev').click(function() {
+    $('body .wrapper .services-list > li#soft-dev > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#mob-apps').click(function() {
+    $('body .wrapper .services-list > li#mob-apps > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#pentest').click(function() {
+    $('body .wrapper .services-list > li#pentest > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#cont-wr').click(function() {
+    $('body .wrapper .services-list > li#cont-wr > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#soc-med').click(function() {
+    $('body .wrapper .services-list > li#soc-med > .inner-list').slideToggle();
+  });
+
+  $('body .wrapper .services-list > li#fine-arts').click(function() {
+    $('body .wrapper .services-list > li#fine-arts > .inner-list').slideToggle();
+  });
+
+
 });
